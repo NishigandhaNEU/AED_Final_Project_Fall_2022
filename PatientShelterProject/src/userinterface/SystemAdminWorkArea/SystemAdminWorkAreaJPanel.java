4b74948cd774ec3c1bb15092f8adba996e59153e
NishigandhaNEU <patel.nishig@northeastern.edu>
2022-12-11 15:04:54 -0500

    added person to sysadmin

--- PatientShelterProject/src/userinterface/SystemAdminWorkArea/SystemAdminWorkAreaJPanel.java
+++ PatientShelterProject/src/userinterface/SystemAdminWorkArea/SystemAdminWorkAreaJPanel.java
@@ -272,7 +272,10 @@ public class SystemAdminWorkAreaJPanel extends javax.swing.JPanel {
 
     private void btnManagePerson1SysActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnManagePerson1SysActionPerformed
         // TODO add your handling code here:
-      
+       ManagePersonJPanel managePersonJPanel = new ManagePersonJPanel(userProcessContainer, system);
+        userProcessContainer.add("managePersonJPanel", managePersonJPanel);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
 
     }//GEN-LAST:event_btnManagePerson1SysActionPerformed
 
